df9ef7f303428a6211daa4c16e4bcc367adb9fe0
Zula <zinowiewa@yandex.ru>
2020-01-12 01:24:26 +0700

    delete linter errors

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -4,24 +4,25 @@ import random
 
 
 def is_even(x):
-    return  'yes' if x % 2 == 0 else 'no'
+    return 'yes' if x % 2 == 0 else 'no'
 
 
 def even():
-    di = {1: random.randint(1, 10), 2: random.randint(1, 100), 3: random.randint(1, 1000)}
+    di = {1: random.randint(1, 10), 2: random.randint(1, 100), 3:
+    random.randint(1, 1000)}
     count = 0
     i = 1
     while i <= 3:
         print("""Question: {}
-Your answer: """.format(di.get(i)), end='')
+        Your answer: """.format(di.get(i)), end='')
         answer = input()
         if answer == is_even(di.get(i)):
-             count +=1
-             print('Correct!')
+            count +=1
+            print('Correct!')
         else:
-             print("""'yes' is wrong answer :(.Correct answer was 'no'.
-Let's try again, Bill!""")
-             break
+            print("'yes' is wrong answer :(.Correct answer was
+            'no'.\nLet's try again, Bill!")
+            break
         i += 1
     if count == 3:
         print('Congratulations, Bill!', end='')
@@ -35,4 +36,4 @@ def main():
 
 
 if __name__ == '__main__':
-     main()
+    main()
