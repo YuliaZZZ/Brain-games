1160302187c36c32c79c3e80ebbe00862237e5a0
Zula <zinowiewa@yandex.ru>
2020-01-29 01:34:44 +0700

    gcd

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -4,18 +4,20 @@ from random import randint
 welcome = 'Find the greatest common divisor of given numbers.'
 
 
+def gcd_check(x, y):
+    s = [x, y]
+    s.sort()
+    a, b = s
+    if b % a == 0:
+        return a
+    i = a
+    while i >= 1:
+        if b % i == 0 and a % i == 0:
+            return i
+        i -= 1
+
+
 def func():
-    def gcd_check(x, y):
-        s = [x, y]
-        s.sort()
-        a, b = s
-        if b % a == 0:
-            return a
-        i = a
-        while i >= 1:
-            if b % i == 0 and a % i == 0:
-                return i
-            i -= 1
     number1 = randint(1, 100)
     number2 = randint(1, 100)
     question = '{} {}'.format(number1, number2)
