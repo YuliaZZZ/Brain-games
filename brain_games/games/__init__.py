b6fd8077195e16992bfce9aa349a2a7d03f99433
Zula <zinowiewa@yandex.ru>
2020-01-31 03:03:41 +0700

    fix imports

---
+++ brain_games/games/__init__.py
@@ -0,0 +1,4 @@
+from brain_games.games import even, calc, gcd, prime, progres   # noqa F401
+
+
+__all__ = ('even', 'calc', 'gcd', 'prime', 'progres')   # noqa F401
--- brain_games/scripts/__init__.py
+++
@@ -1,4 +0,0 @@
-from brain_games.games import even, prime, gcd, progres, calc  # noqa F401
-
-
-__all__ = ('even', 'prime', 'gcd', 'progres', 'calc')   # noqa F401
